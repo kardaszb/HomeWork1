22c07a672d0e1d3babec3386211006c4624bd75d
Bartek Karadsz <bartek.kardasz@gmail.com>
2020-04-02 14:13:17 +0200

    Adding changes to MethodOne.java

--- src/MethodOne.java
+++ src/MethodOne.java
@@ -1,7 +1,6 @@
 public class MethodOne {
     public static void main(String[] args) {
-        System.out.println("Uczę");
-        System.out.println("sie");
+        System.out.println("Uczę\nsie");
         System.out.println("programowania\ni\ncommitowania");
     }
 }
